c76eb2cccd881fbbeffe6a65b370a2428ba827d8
Gosia Wielgus <gosia.wielgus@gmail.com>
2013-01-16 21:56:03 +0100

    Change gencross to return [][]

--- runsim.swift
+++ runsim.swift
@@ -6,15 +6,35 @@ app (stringfile out) bash(string command) {
   bash "-c" command stdout=@out;
 }
 
-(string out[]) gencross(string sets[][]) {
+(string out[][]) gencross(string sets[][]) {
 	string setreg[];	
 	foreach set,i in sets {
 		setreg[i] = @arg("leftb")+@strjoin(set,",")+@arg("rightb");
 	}
 	stringfile tmpfile = bash("echo "+@strjoin(setreg,"_"));
 	string tmpstr = readData(tmpfile);
-	out = @strsplit(tmpstr," ");
+	string[] lines = @strsplit(tmpstr," ");
+	foreach line,i in lines {
+		out[i] = @strsplit(line,"_");
+	}
 }
+
+(string out[][]) project(string sets[][], int dim) {
+	string setsdim[][];	
+	foreach set,i in sets {
+		if(i<dim) {
+			setsdim[i] = set;
+		}
+		if(i>dim) {
+			setsdim[i-1] = set;
+		}
+	}
+	out = gencross(setsdim);	
+}
+
+(int out[]) findindexes(string sets[], string set[], int dim) {	
+}
+
 
 app (datafile out) simulation(string args[]) {
 	echo args stdout=@filename(out);
@@ -31,45 +51,37 @@ app (datafile out) avgcount(datafile files[]) {
 string params[][];
 params[0] = ["5","10"];
 params[1] = ["0.2","0.4"];
-string samples[] = ["0","1","2"];
+# samples
+params[2] = ["0","1","2"];
 
 int lengths[];
 foreach param,i in params {
     lengths[i] = @length(param);
 }
 
-string configparams[][];
-configparams[0] = params[0];
-configparams[1] = params[1];
-configparams[2] = samples;
+string configs[][] = gencross(params);
+trace(configs);
 
-string configs[] = gencross(configparams);
 datafile outfiles[];
-
-foreach config, i in configs {
+foreach config,i in configs {
         datafile f<
                 regexp_mapper;
-                source=config,
+                source=@strjoin(config,"_"),
                 match="(.*)",
                 transform="sim_\\1.dat">;
-        f = simulation(@strsplit(config,"_"));
+        f = simulation(config);
         outfiles[i] = f;
 }
 
-string avgs[] = gencross(params);
+string avgs[][] = project(params,2);
 
 foreach avg in avgs {
-        datafile sample_files[]<
-                structured_regexp_mapper;
-                source=samples,
-                match="(.*)",
-                transform="sim_"+avg+"\\1_summary.dat">;
+	int sampleindexes[] = findindexes(avg,params[2],2);
+	trace(sampleindexes);
         datafile summary<
                 regexp_mapper;
                 source=avg,
                 match="(.*)",
                 transform="sim_\\1_summary.dat">;
+#	summary = avgcount(samplefiles);	
 }
-
-
-
