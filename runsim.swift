9fe9d629132df6641553c45421fb4bf51631d9f7
Gosia Wielgus <gosia.wielgus@gmail.com>
2013-01-17 01:21:08 +0100

    Finding sample files

--- runsim.swift
+++ runsim.swift
@@ -2,6 +2,11 @@ type stringfile;
 type plotfile;
 type datafile;
 
+type projectindex {
+	string params[];
+ 	int indices[];
+}
+
 app (stringfile out) bash(string command) {
   bash "-c" command stdout=@out;
 }
@@ -9,7 +14,11 @@ app (stringfile out) bash(string command) {
 (string out[][]) gencross(string sets[][]) {
 	string setreg[];	
 	foreach set,i in sets {
-		setreg[i] = @arg("leftb")+@strjoin(set,",")+@arg("rightb");
+		if(@length(set)>1) {
+			setreg[i] = @arg("leftb")+@strjoin(set,",")+@arg("rightb");
+		} else {
+			setreg[i] = @strjoin(set,",");
+		}
 	}
 	stringfile tmpfile = bash("echo "+@strjoin(setreg,"_"));
 	string tmpstr = readData(tmpfile);
@@ -19,22 +28,52 @@ app (stringfile out) bash(string command) {
 	}
 }
 
-(string out[][]) project(string sets[][], int dim) {
-	string setsdim[][];	
+(string out[]) indexlist(string set[]) {
+	foreach elem,i in set {
+		out[i]=@strcat(i);
+	}
+}
+
+(int out) findindex(string indices[], int lengths[]) {
+	int tmparray[];
+	tmparray[0] = 0;
+	foreach index,i in indices {
+		tmparray[i+1] = tmparray[i]*lengths[i] + @toint(indices[i]);
+	}
+	out = tmparray[@length(indices)];
+}
+
+(projectindex out[]) project(string sets[][], int dim, int lengths[]) {
+	string indexsets[][];	
 	foreach set,i in sets {
 		if(i<dim) {
-			setsdim[i] = set;
+			indexsets[i] = indexlist(set);
 		}
 		if(i>dim) {
-			setsdim[i-1] = set;
+			indexsets[i-1] = indexlist(set);
 		}
 	}
-	out = gencross(setsdim);	
+	string summaries[][] = gencross(indexsets);	
+	
+	foreach summary,i in summaries {		
+		string newsets[][];
+		foreach param,j in summary {
+			if(j<dim) {
+				newsets[j][0] = param;
+				out[i].params[j] = sets[j][@toint(param)];
+			}
+			if(j>dim) {
+				newsets[j+1][0] = param;
+				out[i].params[j] = sets[j+1][@toint(param)];
+			}
+		}
+		newsets[dim] = sets[dim];
+		string indset[][] = gencross(newsets);
+		foreach inds,k in indset {
+			out[i].indices[k] = findindex(inds,lengths);
+		}
+	}
 }
-
-(int out[]) findindexes(string sets[], string set[], int dim) {	
-}
-
 
 app (datafile out) simulation(string args[]) {
 	echo args stdout=@filename(out);
@@ -49,18 +88,18 @@ app (datafile out) avgcount(datafile files[]) {
 }
 
 string params[][];
-params[0] = ["5","10"];
+params[0] = ["5","10","15"];
 params[1] = ["0.2","0.4"];
+params[2] = ["0.1","0.2"];
 # samples
-params[2] = ["0","1","2"];
+params[3] = ["0","1","2","3"];
 
-int lengths[];
+int lens[];
 foreach param,i in params {
-    lengths[i] = @length(param);
+    lens[i] = @length(param);
 }
 
 string configs[][] = gencross(params);
-trace(configs);
 
 datafile outfiles[];
 foreach config,i in configs {
@@ -73,15 +112,18 @@ foreach config,i in configs {
         outfiles[i] = f;
 }
 
-string avgs[][] = project(params,2);
+
+projectindex avgs[] = project(params,3,lens);
 
 foreach avg in avgs {
-	int sampleindexes[] = findindexes(avg,params[2],2);
-	trace(sampleindexes);
         datafile summary<
                 regexp_mapper;
-                source=avg,
+                source=@strjoin(avg.params,"_"),
                 match="(.*)",
                 transform="sim_\\1_summary.dat">;
-#	summary = avgcount(samplefiles);	
+	datafile samplefiles[];
+	foreach index,i in avg.indices {
+		samplefiles[i] = outfiles[index];
+	}
+	summary = avgcount(samplefiles);	
 }
